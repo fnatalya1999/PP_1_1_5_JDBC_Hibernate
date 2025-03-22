018120c2221ce45d360d8a8e6d8686e7d55bf3c9
Наталья Скворцова <fnatalya1999@mail.ru>
2025-03-23 00:05:39 +0300

    Создана таблица пользователей с использованием hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.util.Util;
+
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        Util.getSessionFactory();
+
+        UserDao daoHibernateImpl = new UserDaoHibernateImpl();
+        daoHibernateImpl.createUsersTable();
+
+        daoHibernateImpl.saveUser("natalia", "faritova", (byte) 30);
+        daoHibernateImpl.saveUser("natalia1", "faritova1", (byte) 31);
+        daoHibernateImpl.saveUser("natalia2", "faritova2", (byte) 32);
+        daoHibernateImpl.saveUser("natalia3", "faritova3", (byte) 33);
+
+        daoHibernateImpl.removeUserById(2);
+        daoHibernateImpl.getAllUsers();
+        daoHibernateImpl.cleanUsersTable();
+        daoHibernateImpl.dropUsersTable();
     }
 }
+
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,43 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static SessionFactory getsessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (getsessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                configuration.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
+                configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/postgres");
+                configuration.setProperty("hibernate.connection.username", "postgres");
+                configuration.setProperty("hibernate.connection.password", "postgres");
+                configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+                configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+                configuration.setProperty("hibernate.show_sql", "true");
+
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                getsessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+                System.out.println("Успешное подключение к БД!");
+            } catch (Exception e) {
+                System.out.println("Ошибка при открытии сессии: " + e.getMessage());
+            }
+        } else {
+            System.out.println("SessionFactory не создано. Проверьте настройки Hibernate.");
+        }
+        return getsessionFactory;
+    }
 }
+
+
+
